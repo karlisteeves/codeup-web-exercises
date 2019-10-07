ce859657c41947ed7bfb95a85cae1dbfb9a3c158
Karli Steeves <steeves.karli@gmail.com>
2019-10-07 13:45:08 -0500

    half way through the JS exercises

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+var message = "Hello from External JS!";
+console.log(message);
+
+alert("Welcome to my website!")
+
+var color = prompt("What's your Favorite Color?");
+alert( "Hey, " + color + " is my favorite color, too!");
+
+
+
