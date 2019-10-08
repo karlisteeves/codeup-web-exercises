febd3847488550488951e5a55311ffb2603fc7d4
Karli Steeves <steeves.karli@gmail.com>
2019-10-08 09:09:25 -0500

    completed morning warm up

--- js/external.js
+++ js/external.js
@@ -1,67 +1,70 @@
 "use strict";
-var message = "Hello from External JS!";
-console.log(message);
-
-alert("Welcome to my website!")
-
-var color = prompt("What's your Favorite Color?");
-if (color === "red") {
-    alert("That's my favorite color, too!");
-} else if (color === "blue") {
-    alert("I hate that color.");
-} else if (color === "pink") {
-    alert("Pink is pretty awesome.");
-} else if (color === "green") {
-    alert ("Green like boogies?!")
-} else if (color === "purple") {
-    alert ("Purple is the color of Royalty!")
-} else {
-    alert("That color is okay.")
-}
-
-
-var daysRented = prompt( "How many days have you had this rental?");
-function priceDay(daysRented){
-    return 3 * daysRented;
-}
-console.log(priceDay(daysRented));
-
-
-
-
+// var message = "Hello from External JS!";
+// console.log(message);
 //
-var googleHours = prompt ("How many hours have you worked for google this week?");
-var amazonHours = prompt ("How many hours have you worked for amazon this week?");
-var facebookHours = prompt  ("How many hours have you worked for Facebook this week?");
-function weekPay (googleHours, amazonHours, facebookHours){
-    var googlePay = 400 * parseInt(googleHours);
-    var amazonPay = 380 * parseInt(amazonHours);
-    var facebookPay = 350 * parseInt(facebookHours);
-
-    return googlePay + amazonPay + facebookPay;
-}
-console.log (weekPay(googleHours, amazonHours, facebookHours));
-
-// //ask about this one it is a tricky thing//
+// alert("Welcome to my website!")
 //
-var availableSpace = true;
-var scheduleSpace = true;
-var enrollmentStatus = availableSpace && scheduleSpace;
-console.log(enrollmentStatus);
-
+// var color = prompt("What's your Favorite Color?");
+// if (color === "red") {
+//     alert("That's my favorite color, too!");
+// } else if (color === "blue") {
+//     alert("I hate that color.");
+// } else if (color === "pink") {
+//     alert("Pink is pretty awesome.");
+// } else if (color === "green") {
+//     alert ("Green like boogies?!")
+// } else if (color === "purple") {
+//     alert ("Purple is the color of Royalty!")
+// } else {
+//     alert("That color is okay.")
+// }
 //
+//
+// var daysRented = prompt( "How many days have you had this rental?");
+// function priceDay(daysRented){
+//     return 3 * daysRented;
+// }
+// console.log(priceDay(daysRented));
+//
+//
+//
+//
+// //
+// var googleHours = prompt ("How many hours have you worked for google this week?");
+// var amazonHours = prompt ("How many hours have you worked for amazon this week?");
+// var facebookHours = prompt  ("How many hours have you worked for Facebook this week?");
+// function weekPay (googleHours, amazonHours, facebookHours){
+//     var googlePay = 400 * parseInt(googleHours);
+//     var amazonPay = 380 * parseInt(amazonHours);
+//     var facebookPay = 350 * parseInt(facebookHours);
+//
+//     return googlePay + amazonPay + facebookPay;
+// }
+// console.log (weekPay(googleHours, amazonHours, facebookHours));
+//
+// // //ask about this one it is a tricky thing//
+// //
+// var availableSpace = true;
+// var scheduleSpace = true;
+// var enrollmentStatus = availableSpace && scheduleSpace;
+// console.log(enrollmentStatus);
+//
+// //
+//
+//
+// var numberItems = 1;
+// var offerValid = true;
+// var premiumMember = true;
+// var purchaseProduct = (premiumMember || numberItems >= 2) && offerValid;
+// console.log(purchaseProduct);
+//
+//
+//
+//
+// var username= 'codeup';
+// var password = 'notastrongpassword';
+//
+// password.length >= 5 && password.indexOf(username) === -1 && username.length <= 20 && username === username.trim() && password === password.trim();
 
 
-var numberItems = 1;
-var offerValid = true;
-var premiumMember = true;
-var purchaseProduct = (premiumMember || numberItems >= 2) && offerValid;
-console.log(purchaseProduct);
-
-
-
-
-var username= 'codeup';
-var password = 'notastrongpassword';
-
-password.length >= 5 && password.indexOf(username) === -1 && username.length <= 20 && username === username.trim() && password === password.trim();
+// declare a global variable
