8a5de2867b15b9224640c6a36d3467afcb1d21a8
Karli Steeves <steeves.karli@gmail.com>
2019-10-08 10:00:05 -0500

    some function practice, named vs anonymous

---
+++ js/functionspractice.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+
+function divideNumber (x, y){
+    return (x / y);
+
+}
+
+console.log(divideNumber(10, 2));
+
+
+function multiplyNumber (x, y){
+    return (x * y);
+}
+
+console.log(multiplyNumber (2, 10));
+
+
+var divideNumber = function(x, y){
+    return (x / y);
+}
+
+console.log(divideNumber(20,2));
+
+var multNumber = function(x, y){
+    return (x * y);
+}
+
+console.log(multNumber(20,2));
