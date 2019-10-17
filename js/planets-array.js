6ba60458df275ef2eded343bd8b4ca080b98e10b
Karli Steeves <steeves.karli@gmail.com>
2019-10-17 13:48:19 -0500

    went over the js function

--- js/planets-array.js
+++ js/planets-array.js
@@ -17,15 +17,20 @@
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
-
+    planets.unshift("The Sun");
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+
+    planets. push("Pluto");
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+
+    var takingoff = planets.shift("The Sun");
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
+
 
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
