8cdfd39ce0e76800cfcc803dc3466d907eeaf976
Karli Steeves <steeves.karli@gmail.com>
2019-10-21 10:24:14 -0500

    adding loops html

---
+++ js/circle.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            var area = (Math.PI * (circle.radius * circle.radius));
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return area; // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            if (doRounding === true){
+                var newNum = Math.round(circle.getArea());
+
+            }else newNum = circle.getArea();
+
+
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + newNum);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
