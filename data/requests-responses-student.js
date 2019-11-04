eb1d4074b62832a3e5488779206fefa2144c5a32
Karli Steeves <steeves.karli@gmail.com>
2019-11-04 15:53:31 -0600

    working on ajax exercises

--- data/requests-responses-student.js
+++ data/requests-responses-student.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
      * TO DO TOGETHER: Let's make our first AJAX request. Generate a new Hookbin
      * endpoint, then query it for a username...
      */
+    //
+    // console.log($.ajax("https://hookbin.com/ggeJQzxw7XFlL30dV6a2?username=Bob"));
 
 
 
@@ -18,23 +20,31 @@ $(document).ready(function(){
      * following JavaScript Object to your Hookbin AJAX request:
      */
 
-    var person = {
-        type: "POST",
-        data: {
-            first: "Jesse",
-            last: "Pinkman",
-            location: "unknown"
-        }
-    };
-
-    // console.log($.ajax());
+    // var person = {
+    //     type: "POST",
+    //     data: {
+    //         first: "Jesse",
+    //         last: "Pinkman",
+    //         location: "unknown"
+    //     }
+    // };
+    //
+    // console.log($.ajax("https://hookbin.com/ggeJQzxw7XFlL30dV6a2", person));
 
     /*
      * TO DO: Refactor the first example using a GET request object instead of
      * appending a query to the url.
-     */
-
-
+   //   */
+   //
+   //
+   //      var bob = {
+   //          type: "GET",
+   //          data: {
+   //              username: "Bob"
+   //          }
+   //
+   //  };
+   // console.log( $.ajax("https://hookbin.com/ggeJQzxw7XFlL30dV6a2"));
     /*********************************************
      *              REQUESTS and RESPONSES
      ******************************************** */
@@ -52,7 +62,9 @@ $(document).ready(function(){
      * TO DO: Look up the Star Wars API and make a similar request that would
      * return a list of all Star Wars films.
      */
-
+    // var starWars= $.ajax("https://swapi.co/api/people/").done(function(data){
+    //     console.log(data);
+    // });
     /*
      * That's fine and dandy, but what if we have a local JSON file and
      * want to request data from that? We can! The URL will be the path to
@@ -61,29 +73,43 @@ $(document).ready(function(){
      * TO DO TOGETHER: Let's make a request to the books inventory we saved
       * previously.
      */
+ var myBooks =$.ajax("data/books.json");
+ myBooks.done(function(data){
+     console.log(data);
+ });
 
-
+ // myBooks.always(function(){
+ //     console.log("Book search complete!");
+ // });
     /*
      * TO DO TOGETHER: What if we want to display a message if this AJAX request
      * fails?
      */
-
+// starWars.fail(function () {
+//     console.log("These are not the androids you are looking for.")
+// });
     /*
      * TO DO TOGETHER: How about a function that always runs whether the request
      * fails or not?
      */
-
+    // starWars.always(function () {
+    //     console.log("Long ago, in a galaxy far, far away...")
+    // });
     /*
      * TO DO: Refactor your Star Wars API request to log a message that says
      * "Something wrong with your request..." if it fails.
      */
-
+    // starWars.fail(function (jqXhr, status) {
+    //     console.log("Something wrong with your request...")
+    // });
     /*
      * TO DO: Refactor your Star Wars API request to log a message that says
      * "...loading" whether the request fails or not.
      */
 
-
+    // starWars.always(function () {
+    //     console.log("...loading")
+    // });
     /*
      * TO DO TOGETHER: Create a Star Wars API request that queries for "A
      * New Hope" and store this request in a variable.
@@ -91,17 +117,38 @@ $(document).ready(function(){
      * Take a look at the object that is being returned. Write a console log
       * that displays the director of the film.
      */
-
+  var myMovie= $.ajax("https://swapi.co/api/films/", {
+      type: "GET",
+      data: {
+          "search": "A New Hope"
+      }
+  });
+      myMovie.done(function(data){
+          console.log("the Director of A New Hope is: " + data.results[0].director);
+      });
     /*
      * TO DO: Create a new variable that makes a similar request. Search for
      * "The Force Awakens" and console log its release date.
      */
 
+    var forceAwakens = $.ajax("https://swapi.co/api/films/", {
+        type: "GET",
+        data: {
+            "search": "The Force Awakens"
+        }
+    });
+        forceAwakens.done(function (data) {
+            console.log("This film's release date is: " +  data.results[0].release_date);
+
+        });
     /*
      * TO DO: Make a request to books.json. Return the author of "The
      * Canterbury Tales."
      */
 
+     myBooks.done(function(data){
+         console.log(data[17].author);
+     });
 
 
     /*********************************************
@@ -117,12 +164,14 @@ $(document).ready(function(){
      * a request to a server!
      */
 
+
     /*
      * TO DO TOGETHER: There's some additional refactoring we can do. We can
      * break up our callback methods on an AJAX request.
      *
      * We can also declare named functions to pass in to these methods.
      */
+
 
     /*********************************************
      *              AJAX and HTML
@@ -134,9 +183,24 @@ $(document).ready(function(){
      * class/id to target it.
      */
 
+    var requestBooks = $.ajax("data/books.json");
+
+ $.each (data, function(index, book){
+     var content += "<h2>"
+     content += book.title;
+     content += "</h2>"
+
+     console.log(book.title);
+ })
+
+
+    requestBooks.fail(function(data){
+        console.log("Something went wrong.");
+    })
     /*
      * TO DO: Add your favorite book to the end of books.json.
      */
+
 
     /*
      * Bonus: Create a button the refreshes the contents of your html
