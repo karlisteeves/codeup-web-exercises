b6be004bb55f6b1fbe0e0f0f891061f8da6149c2
Karli Steeves <steeves.karli@gmail.com>
2019-11-06 16:08:40 -0600

    weather map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -34,6 +34,9 @@ for (var i = 0; i < inputs.length; i++) {
     inputs[i].onclick = switchLayer;
 }
 
+
+
+//how to place my marker on the map//
 var newMarker = new mapboxgl.Marker();
 newMarker.setLngLat([-98.4916, 29.4252]);
 newMarker.addTo(map);
@@ -46,23 +49,26 @@ function getWeather() {
     $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7432dcc353e65f1a1ddbe0448d1b5477/" + newMarker.getLngLat().lat + "," + newMarker.getLngLat().lng).done(function (data) {
         $('.day').each(function (i) {
             $(this).html("");
+            // $(this).append("<img style='height: 50px; width: 50px' alt='' src='" + conditions[data.daily.data[i].icon] + "'>");
             $(this).append("<h2>" + Math.round(data.daily.data[i].temperatureHigh) + "&deg / " + Math.round(data.daily.data[i].temperatureLow) + "&deg</h2>");
             $(this).append("<p>Summary: " + data.daily.data[i].summary +"</p>");
             $(this).append("<p>Precipitation: " + data.daily.data[i].precipType + "</p>");
             $(this).append("<p>Wind: " + data.daily.data[i].windSpeed + "</p>");
+
+
         });
     });
 }
-//
-// $('#search').click(function () {
-//     geocode($('#searchInput').val(), mapboxToken).then(function (result) {
-//         newMarker.setLngLat(result);
-//
-//         map.flyTo({center: result, zoom: 5});
-//         getWeather();
-//     });
-//     $('#searchInput').val("");
-// });
+
+$('#search').click(function () {
+    geocode($('#searchInput').val(), mapboxToken).then(function (result) {
+        newMarker.setLngLat(result);
+
+        map.flyTo({center: result, zoom: 5});
+        getWeather();
+    });
+    $('#searchInput').val("");
+});
 
 newMarker.on('dragend', getWeather);
 
